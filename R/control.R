c40d89ee72a6dba4364ffd10cb39b95da8918d7a
zq2323 <782903342@qq.com>
2023-04-15 19:47:52 +0800

    Update control.R

--- R/control.R
+++ R/control.R
@@ -1,5 +1,5 @@
-index_file <- "index.html"
-pagecryptr::pagecryptr(file, "r", out_file = "index.html")
+# Encrypt an HTML Page by pagecryptr
+pagecryptr::pagecryptr("index.html", "r", out_file = "index.html")
 contents <- paste(readLines("index.html"), collapse = "\n")
 writeLines(gsub('<div id=\"attribution\">\n        Protected by <a href=\"https://www.maxlaumeister.com/pagecrypt/\">PageCrypt</a>\n    </div>', "", contents), "index.html")
 for (i in list.dirs('posts')[-1]) {

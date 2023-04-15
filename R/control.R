cc1bef40257256a192b422b99ef82bfb770da2e4
zq2323 <782903342@qq.com>
2023-04-15 18:32:25 +0800

    Update control.R

--- R/control.R
+++ R/control.R
@@ -1,8 +1,8 @@
-index_file <- "_site/index.html"
-pagecryptr::pagecryptr(file, "r", out_file = "_site/index.html")
-contents <- paste(readLines("_site/index.html"), collapse = "\n")
-writeLines(gsub('<div id=\"attribution\">\n        Protected by <a href=\"https://www.maxlaumeister.com/pagecrypt/\">PageCrypt</a>\n    </div>', "", contents), "_site/index.html")
-for (i in list.dirs('_site/posts')[-1]) {
+index_file <- "index.html"
+pagecryptr::pagecryptr(file, "r", out_file = "index.html")
+contents <- paste(readLines("index.html"), collapse = "\n")
+writeLines(gsub('<div id=\"attribution\">\n        Protected by <a href=\"https://www.maxlaumeister.com/pagecrypt/\">PageCrypt</a>\n    </div>', "", contents), "index.html")
+for (i in list.dirs('posts')[-1]) {
     posts <- list.files(i)
     for (j in posts) {
         file <- file.path(i, j)
